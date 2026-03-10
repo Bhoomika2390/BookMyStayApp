b7f3cb7d2cd617e774ffd89372b66ebd698fb820
Bhoomika2390 <bhoomikapoddar06@gmail.com>
2026-03-10 15:23:47 +0530

    Bhoomika Added feature/UC1-WelcomePage

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,17 @@
+/**
+ * Book My Stay Application
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * @author Bhoomika
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking System");
+        System.out.println("Application started successfully.");
+
+    }
+}
