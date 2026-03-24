1f415623cf166376b355ff145811e5af9d104e84
Bhoomika Poddar <bhoomikapoddar@Bhoomikas-MacBook-Air.local>
2026-03-24 20:10:11 +0530

    Added UC6 Room Allocation

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -14,21 +14,49 @@ public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Booking Request Queue\n");
+        System.out.println("Room Allocation Processing\n");
 
+        // Step 1: Queue (UC5)
         Queue<Reservation> bookingQueue = new LinkedList<>();
 
-        // Add booking requests
         bookingQueue.add(new Reservation("Abhi", "Single"));
-        bookingQueue.add(new Reservation("Subha", "Double"));
+        bookingQueue.add(new Reservation("Subha", "Single"));
         bookingQueue.add(new Reservation("Vanmathi", "Suite"));
 
-        // Process booking requests (FIFO)
-        while (!bookingQueue.isEmpty()) {
-            Reservation r = bookingQueue.poll();  // removes first element
+        // Step 2: Inventory
+        Map<String, Integer> inventory = new HashMap<>();
+        inventory.put("Single", 2);
+        inventory.put("Suite", 1);
 
-            System.out.println("Processing booking for Guest: "
-                    + r.guestName + ", Room Type: " + r.roomType + "\n");
+        // Step 3: Track room count per type
+        Map<String, Integer> roomCount = new HashMap<>();
+
+        // Step 4: Process queue
+        while (!bookingQueue.isEmpty()) {
+
+            Reservation r = bookingQueue.poll();
+            String type = r.roomType;
+
+            if (inventory.get(type) > 0) {
+
+                // Increment count for that type
+                roomCount.put(type, roomCount.getOrDefault(type, 0) + 1);
+
+                int count = roomCount.get(type);
+
+                // Generate Room ID like Single-1, Suite-1
+                String roomId = type + "-" + count;
+
+                // Reduce inventory
+                inventory.put(type, inventory.get(type) - 1);
+
+                System.out.println("Booking confirmed for Guest: "
+                        + r.guestName + ", Room ID: " + roomId + "\n");
+
+            } else {
+                System.out.println("Booking failed for Guest: "
+                        + r.guestName + " (No rooms available)\n");
+            }
         }
     }
 }
